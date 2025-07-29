6bf072d7f8630a834b490a650f20b41fd82b1ff2
redotomi <tomy.redondo001@gmail.com>
2025-07-29 17:19:05 -0300

    ADD: Endless ground functionality

--- Scenes/ground.gd
+++ Scenes/ground.gd
@@ -1,3 +1,21 @@
 extends Node2D
 
 class_name Ground
+
+@export var speed = -150
+
+@onready var sprite1: Sprite2D = $Ground1/Sprite2D
+@onready var sprite2: Sprite2D = $Ground2/Sprite2D
+
+func _ready() -> void:
+	sprite2.global_position.x = sprite1.global_position.x + sprite1.texture.get_width()
+	
+func _process(delta: float) -> void:
+	sprite1.global_position.x += speed * delta
+	sprite2.global_position.x += speed * delta
+	
+	if sprite1.global_position.x < -sprite1.texture.get_width():
+		sprite1.global_position.x = sprite2.global_position.x + sprite2.texture.get_width()
+		
+	if sprite2.global_position.x < -sprite2.texture.get_width():
+		sprite2.global_position.x = sprite1.global_position.x + sprite2.texture.get_width()
